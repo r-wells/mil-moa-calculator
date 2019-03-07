b7fa4115d36f9a144f7de673ce498c6601360828
Ryan <r.l.wells@outlook.com>
2019-03-07 09:01:43 -0600

    added selected functionality

--- src/MOA/Distance/Distance.js
+++ src/MOA/Distance/Distance.js
@@ -7,7 +7,7 @@ const distance = (props) => {
             <h4>Mils To MOA Calculator</h4>
             <p>Enter distance to Target:</p>
             <input type="text" onChange={props.changed} />
-            <p className="value-output">Your 1 MOA at <strong><u> {props.distance}</u> yards</strong> MOA is: {props.value} <strong>inches</strong></p>
+            <p className="value-output">1 MOA at <strong><u> {props.distance}</u></strong> is: <strong>{props.value}</strong></p>
         </div>
     )
 }
--- src/MOA/MOA.js
+++ src/MOA/MOA.js
@@ -5,21 +5,43 @@ import './MOA.css';
 class Moa extends Component {
     
   state = {
-    value: ''
+    value: '',
+    units: ''
+  }
+
+  changeHandler = (event) => {
+    this.setState(
+        {units: event.target.value}
+    )
+    console.log(this.state.units);
   }
 
   calculateMoa = (event) => {
-    this.setState(
-        {value: ((event.target.value / 100) * 1.047).toFixed(4),
-        distance: event.target.value}
-    );
+
+    if(this.state.units === 'yards') {
+        this.setState(
+            {value: ((event.target.value / 100) * 1.047).toFixed(4) + ' inches',
+            distance: event.target.value + ' yards'}
+        );
+    } if(this.state.units === 'meters') {
+        this.setState(
+            {value: ((event.target.value / 100) * 1.047).toFixed(4) + ' centimeters',
+            distance: event.target.value + ' meters'}
+        );
+    }
   }
 
     render() {
     return (
-        <div>
+        <div className="moa-container">
             <h3>MOA Calculations</h3>
             <p>This section is used for MOA Calculations. Use it to calculate distance, Mils to MOA, target size, etc.</p>
+            <p>Choose your units of measurement</p>
+            <select onChange={this.changeHandler} value={this.state.value}>
+                <option value='blank'></option>
+                <option value='yards'>Yards</option>
+                <option value='meters'>Meters</option>
+            </select>
             <Distance
             distance={this.state.distance}
             value={this.state.value}
